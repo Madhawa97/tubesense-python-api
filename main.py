fb0d7d119b2321349b026e901491c2f55c5c1c89
Madhawa Monarawila <madawammonarawila@gmail.com>
2023-06-25 02:22:22 +0530

    Changed code to receive only the comment and send only the perception value

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
 import poe
 import os
+import re
 
 token = os.getenv('POE_TOKEN')
 client = poe.Client(token)
@@ -17,15 +18,46 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+
 class Body(BaseModel):
-    prompt: str
+    comments: str
+
 
 @app.post('/')
-def root(body:Body):
+def root(body: Body):
 
-    prompt = body.prompt
+    comments = body.comments
+
+    if not comments:
+        return {"error": "comments not present."}
+
+    received_response = get_preception(comments)
+    
+    if not received_response:
+        return {"error": "connection with poe failed."}
+    
+    extracted_preception = extract_preception(received_response)
+
+    if not extracted_preception:
+        return {"error": "preception extraction failed."}
+
+    return {"result": extracted_preception}
+
+
+def get_preception(comments):
+    prompt = '''# Preception \n Please analyze the following comments from a YouTube video and rate the perception of the video on a scale of 1 to 5, where 1 is very bad, 2 is bad, 3 is neutral, 4 is good, and 5 is very good. The perception should reflect the overall sentiment, tone, and attitude of the comments towards the video. Please output your answer in the format of { "perception":"X" }, where X is the perception score. \n # Comments \n''' + \
+        comments + \
+        '''\n # Important \n Please output your answer in the format of { "perception":"X" }, where X is the perception score.'''
     for chunk in client.send_message("a2", prompt):
         pass
-    print(chunk["text"])
-
     return chunk["text"]
+
+
+def extract_preception(response_string):
+    match = re.search(r"perception.*?(\d)", response_string)
+
+    if match:
+        perception = match.group(1)
+        return perception
+    else:
+        return None
